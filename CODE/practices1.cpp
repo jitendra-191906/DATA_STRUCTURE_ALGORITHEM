82942d5547c25ea5e286e7385d2aceaa1608ba54
Jitendra kumar <79419900+jitendra-191906@users.noreply.github.com>
2022-01-01 03:12:15 +0530

    dsa fundamentals

---
+++ CODE/practices1.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<istream>
+using namespace std;
+int main(){
+
+    /*data type casting
+    int a= 12;
+    cout<<a<<endl;
+
+    int b = 'r';
+    cout<< b <<endl;
+
+char ch = -12;
+    cout <<ch <<endl;
+    int size = sizeof(ch);
+    cout<< size<<endl;*/
+/* operators
+int c=2.0/5;
+cout<<c<<endl;
+float b=2.0/5;
+cout<<b<<endl;
+
+//for claculate value of space and enter and tab 
+int a;
+a=cin.get();
+cout<<"value of a is "<<a<<endl;
+
+
+int a =2;
+int b = a+1;
+if((a=3)==b){
+    cout<<a;
+
+}
+
+else{
+   cout<<a+1;
+}
+int a =24;
+if (a>20){
+    cout<<"love";
+
+}
+else if (a==24){
+    cout<<"lovely";
+}
+else{
+    cout<<"Babber";
+}
+
+
+for(int i=4;i>=0;i--)
+        {
+            
+            for(int j=4;j>=i+1;j--)
+            {
+          cout<<" * ";
+            }
+       cout<<"\n\n";
+        }
+    }
+*/
+}
