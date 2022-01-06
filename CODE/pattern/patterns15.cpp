1df278a94889699e6e7622a838891194dd8cfe2a
jitendra kumar <jitendrakumarkct01@gmail.com>
2022-01-06 16:27:20 +0530

    bitop. file add

--- CODE/pattern/patterns15.cpp
+++ CODE/pattern/patterns15.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n ;
-    cout<<"enter no of row in patterns";
-    cin>>n;
-    int row =1;
-    while(n<=row){
-        int col =1;
-        int star = n-row + 1;
-        while(star){
-            cout<<star<<" ";
-            star--;
-        }
-        cout<<endl;
-        row++;
+int main()
+{
+    int n;
+    cout << "enter no of row in patterns";
+    cin >> n;
+    int row = 1;
+    while (n <= row)
+    {
 
+        int space = n - row + 1;
+        while (space)
+        {
+            cout << "*";
+            space--;
+        }
+        cout << endl;
+        row++;
     }
 }
