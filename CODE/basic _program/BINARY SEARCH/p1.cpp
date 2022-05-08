47f31e61c875e15e6329bdddd35ab2efa7def8db
jitendra kumar <jitendrakumarkct01@gmail.com>
2022-05-08 19:00:19 +0530

    binary

--- CODE/basic _program/BINARY SEARCH/p1.cpp
+++ CODE/basic _program/BINARY SEARCH/p1.cpp
@@ -1,47 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int size, int key) {
-
+int binarySearch(int arr[], int size, int key)
+{
     int start = 0;
-    int end = size-1;
-
-    int mid = start + (end-start)/2;
-
-    while(start <= end) {
-
-        if(arr[mid] == key) {
+    int end = size - 1;
+    int mid = start + (end - start) / 2;
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
             return mid;
         }
+        // go to right hand side
 
-        //go to right wala part
-        if(key > arr[mid]) {
+        if (key > arr[mid])
+        {
             start = mid + 1;
         }
-        else{ //key < arr[mid]
+        else
+        {
             end = mid - 1;
         }
-
-        mid = start + (end-start)/2;
+        mid = start + end  / 2;
     }
-    
     return -1;
 }
+int main()
+{
 
-
-int main() { 
-
-    int even[6] = {2,4,6,8,12,18};
+    int even[6] = {2, 4, 6, 8, 12, 18};
     int odd[5] = {3, 8, 11, 14, 16};
 
     int evenIndex = binarySearch(even, 6, 6);
 
     cout << " Index of 6 is " << evenIndex << endl;
 
-    int oddIndex = binarySearch(odd, 5, 14);
+    int oddIndex = binarySearch(odd, 5, 1);
 
     cout << " Index of 14 is " << oddIndex << endl;
-
 
     return 0;
 }
