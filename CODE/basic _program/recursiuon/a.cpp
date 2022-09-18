f32e52535e4aa529a5a5d7bccf397701d7abf11b
jitendra kumar <jitendrakumarkct01@gmail.com>
2022-09-18 15:07:08 +0530

    update

--- CODE/basic _program/recursiuon/a.cpp
+++ CODE/basic _program/recursiuon/a.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
+#include<string>
 using namespace std;
+string satdigit(int n,string arr){
+ // base case
+ if(n==0){
+    return ;
+ }
+//  processing
+int  digit =n%10;
+n =n/10;
+// RR
+satdigit(n,arr);
+cout<<arr[digit]<<endl;
+}
 int main(){
     int n;
     cin>>n;
-    string arr[10]={
-         "one","two","three","fo ur","five","six","seven","eight","nine"
-    };
-    
-
-}nStairs
+    string arr[10]={"one","two","three","fo ur","five","six","seven","eight","nine"};
+ satdigit(n, arr) ;
+}
